0f395035808f63bb5cb37928f32d35125e8493f2
Anton Plietzsch <plietzsch@pik-potsdam.de>
2022-05-27 17:23:06 +0200

    ieee variation plot

--- network/mixed_ieee_variation.jl
+++ network/mixed_ieee_variation.jl
@@ -340,14 +340,18 @@ ca = parse.(Measurement{Float64},df.ca)
 min_v = map(x -> stability_interval(x)[1:2], sf_interval)
 
 # simple plot
-p = plot(sf_interval, c, c=:black, xguide=L"$f_S$", yguide=L"\mu_1", ylims=(0, 1), label="original system", legend=:bottomleft, right_margin=15Plots.mm)
+p = plot(sf_interval, c, c=:black, xguide=L"$f_S$", yguide=L"\mu_1", ylims=(0, 1), label="original system", legend=:bottomleft)
 plot!(p, sf_interval,ca, c=:black, ls=:dash, label="normal form")
 p2 = twinx()
-plot!(p2, sf_interval, first.(min_v), yguide=L"\rho_{min}\;[pu]", ylim=(0,1), c=:red, label=false, guidefontcolor=:red)
+plot!(p2, sf_interval, first.(min_v), yguide=L"\rho_{min}\;(pu)", ylim=(0,1), c=:red, label=false, guidefontcolor=:red, ytickfontcolor=:red)
 plot!(p2, sf_interval, last.(min_v), c=:red, ls=:dash, label=false)
-plot!(tickfont=12, 
-    guidefont=14, 
-    legendfont=12)
+plot!(tickfont=14, 
+    guidefont=16, 
+    legendfont=14,
+    gridalpha=0.,
+    right_margin=20Plots.mm,
+    bottom_margin=3Plots.mm,
+    fontfamily="Computer Modern")
 # plot!(sf_interval, df.f, c=:blue, label="ω")
 # plot!(sf_interval, df.fa, c=:blue, ls=:dash, label=false)
 # plot!(sf_interval, df.a, c=:red, label="ρ")
