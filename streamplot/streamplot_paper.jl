81f9c90c5e9b27b507587bfc4a454931c64f5c40
Anton Plietzsch <plietzsch@pik-potsdam.de>
2022-05-30 16:19:22 +0200

    add colorbar to streamplots

--- streamplot/streamplot_paper.jl
+++ streamplot/streamplot_paper.jl
@@ -1,17 +1,12 @@
 ## This is the script for the streamplots in the paper.
 
 using GLMakie
+using LaTeXStrings
 
 ##
 
 plot_path = "$(@__DIR__)/../figures/"
-fig = Figure(resolution=(750, 750),fontsize=30, font="CMU Serif")
-ax = Axis(fig[1, 1], xlabel = L"\Re(u)", ylabel = L"\Im(u)")
-
-## deviations of active and reactive power
-
-δp = 0
-δq = 0.7
+fig = Figure(resolution=(1500, 700),fontsize=30, font="CMU Serif")
 
 ## normal form parameters (no internal variable)
 
@@ -22,19 +17,69 @@ Hr = 1.0; Hi = 0.0
 
 ## normal form model
 
-model(u,δs) = Point2f0(
+model(u,δs) = Point2f(
     Ar*u[1] - Ai*u[2] + Cr*(u[1]^2 + u[2]^2)*u[1] - Ci*(u[1]^2 + u[2]^2)*u[2] + Gr*δs[1]*u[1] - Gi*δs[1]*u[2] + Hr*δs[2]*u[1] - Hi*δs[2]*u[2],
     Ar*u[2] + Ai*u[1] + Cr*(u[1]^2 + u[2]^2)*u[2] + Ci*(u[1]^2 + u[2]^2)*u[1] + Gr*δs[1]*u[2] + Gi*δs[1]*u[1] + Hr*δs[2]*u[2] + Hi*δs[2]*u[1],
 )
 
-## fix δp and δq to the values above
+## first plot
 
+δp = 0.
+δq = 0.
 obs = Base.Fix2(model,[δp,δq])
 
-## streamplot with GLMakie
+ax1 = Axis(fig[1, 1], xlabel = L"\Re(u) \;(pu)", ylabel = L"\Im(u) \;(pu)",aspect = 1)
+sp = streamplot!(obs,
+                -1.5..1.5, -1.5..1.5,
+                gridsize = (20, 20),
+                arrow_size = 0.07,
+                linewidth = 2.5,
+                colormap = :inferno,
+                colorrange = (0,6),
+                gridalpha=0.)
+hidedecorations!(ax1, ticks=false, ticklabels = false, label = false)
 
-sp = streamplot!(obs,-1.5..1.5, -1.5..1.5, gridsize = (20, 20), arrow_size = 0.07, linewidth = 2.5, colormap = :magma)
-hidedecorations!(ax, ticks=false, ticklabels = false, label = false)
-#Colorbar(fig[2,1],vertical=false, flipaxis = false, colormap = :magma)
-display(fig)
-save(plot_path * "streamplot3.png",fig)
+## second plot
+
+δp = -0.9
+δq = 0.
+obs = Base.Fix2(model,[δp,δq])
+
+ax2 = Axis(fig[1, 2], xlabel = L"\Re(u) \;(pu)", ylabel = L"\Im(u) \;(pu)", aspect = 1)
+sp = streamplot!(obs,
+                -1.5..1.5, -1.5..1.5,
+                gridsize = (20, 20),
+                arrow_size = 0.07,
+                linewidth = 2.5,
+                colormap = :inferno,
+                colorrange = (0,6))
+hidedecorations!(ax2, ticks=false, ticklabels = false, label = false)
+
+## third plot
+
+δp = 0
+δq = 0.7
+obs = Base.Fix2(model,[δp,δq])
+
+ax3 = Axis(fig[1, 3], xlabel = L"\Re(u) \;(pu)", ylabel = L"\Im(u) \;(pu)", aspect = 1)
+sp = streamplot!(obs,
+                -1.5..1.5, -1.5..1.5,
+                gridsize = (20, 20),
+                arrow_size = 0.7,
+                linewidth = 2.5,
+                colormap = :inferno,
+                colorrange = (0,6))
+hidedecorations!(ax3, ticks=false, ticklabels = false, label = false)
+
+## add colorbar
+
+Colorbar(fig[2, 1:3],
+        limits = (0,6),
+        colormap=:inferno,
+        vertical=false,
+        flipaxis = false,
+        width = 800,
+        size = 20,
+        label=L"\frac{du}{dt} \; \left(\frac{pu}{s}\right)")
+
+save(plot_path * "streamplot.png",fig)
